ef7b4e2355a728d4be4a5b52067df4a55980d93a
Shahruf hasan <info.shahrufhasan@gmail.com>
2025-11-10 21:58:30 -0500

    connected to mongodb

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://studyMate:acRoIV10oIB7lLj8@cluster0.cnv9fix.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    // All api will go here
+    const db = client.db("studyMatedb");
+    const studentCollection = db.collection("students");
+
+    app.get("/students", async (req, res) => {
+      const result = await studentCollection.find().toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDBsss!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
